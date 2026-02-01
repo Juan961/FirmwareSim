355e7c373f1ecc8a79e7c46802b174e6026f8617
Juan Jose <juanjo5792@gmail.com>
2026-01-31 19:05:22 -0500

    Ensure path to certificates is correct using absolute

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+import os
+
+_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 MQTT_CONFIG = {
-    "CLIEND_ID": "firmware",
+    "CLIENT_ID": "thing",
     "ENDPOINT": "a383zn5ixngw87-ats.iot.us-east-1.amazonaws.com",
-    "PATH_TO_CERT": "./certificates/thing.cert.pem",
-    "PATH_TO_PUBLIC_KEY": "./certificates/thing.public.key",
-    "PATH_TO_PRIVATE_KEY": "./certificates/thing.private.key",
+    "PATH_TO_CERT": os.path.join(_SCRIPT_DIR, "certificates", "thing.cert.pem"),
+    "PATH_TO_PUBLIC_KEY": os.path.join(_SCRIPT_DIR, "certificates", "thing.public.key"),
+    "PATH_TO_PRIVATE_KEY": os.path.join(_SCRIPT_DIR, "certificates", "thing.private.key"),
 
     "COMMAND_TOPIC_SUB": "device/thing/command",
 
